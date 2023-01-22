eb27bdf017a85b40fad3e21534204b0fcc58db31
Jettology <bsi.s.park@gmail.com>
2023-01-22 02:47:44 -0600

    Implement Fire Blast attack animation

--- index.js
+++ index.js
@@ -6,14 +6,36 @@
 // Buttons
 const btnStart = document.querySelector("#startbutton");
 
+const btnTest = document.querySelector("#TESTBUTTON");
+
+// Images
+let playerHpImage = document.querySelector(".player-hp-image");
+let playerDataImage = document.querySelector(".player-data-image");
+let opponentHpImage = document.querySelector(".opponent-hp-image");
+let attackImage = document.querySelector(".attack-image");
+
 // Div
 const divStart = document.querySelector(".start-container");
 const divIntro = document.querySelector(".intro-container");
 const divContainer = document.querySelector(".container");
 
 // ======================================================================
+// Other variables
+// ======================================================================
+const attackDuration = 2400;
+const hpDepletionDuration = 1200;
+
+
+// ======================================================================
 // Check for "START" and transition to animation
 // ======================================================================
+
+// temp: get rid of START animation for faster testing
+divStart.style.display = "none";
+divIntro.style.display = "none";
+divContainer.style.display = "flex";
+
+
 
 btnStart.addEventListener('click', transitionIntro);
 
@@ -24,6 +46,38 @@ function transitionIntro() {
     divContainer.style.display = "flex";    // show main game container
 }
 
+// =============
+// TEST BUTTON
+// =============
+btnTest.addEventListener('click', () => {
+    // Animation to deplete HP to 0
+    depleteHpAnimation(playerHpImage);
+
+    // Changes databox from Full HP to Empty HP
+    changeDataboxHp(playerDataImage);
+
+    // same for opponent's hp
+    depleteHpAnimation(opponentHpImage);
+
+    // show pokemon move aniation
+    attackImage.style.display = "block";
+    attackImage.style.animation = `moveSpriteSheet_Y ${attackDuration}ms steps(26)`;
+    setTimeout(() => { attackImage.style.display = "none"; }, attackDuration);
+    // attackImage.style.display = "none";
+});
+
+function depleteHpAnimation(trainerHpImage) {
+    trainerHpImage.style.animation = `deplete-hp ${hpDepletionDuration}ms`;
+    trainerHpImage.style.width = "0%";
+}
+
+// Changes the databox for the PLAYER to account for HP changes
+// Note: OPPONENT does not need this method with current databox UI
+function changeDataboxHp(trainerDataImage) {
+    let oldImage = trainerDataImage.src;
+    let newImage = oldImage.slice(0, oldImage.length - 4) + "-Fainted.png";
+    setTimeout(() => { trainerDataImage.src = newImage; }, hpDepletionDuration);
+}
 
 
 // btn.addEventListener('click', function () {
