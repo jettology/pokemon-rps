1723a67369a5f367c7258d45e8ad51493b28b7e2
Jettology <bsi.s.park@gmail.com>
2023-01-27 01:42:04 -0600

    Add pokemon highlights and pokeball effects

--- index.js
+++ index.js
@@ -3,21 +3,43 @@
 // Variable instantiations
 // ======================================================================
 
+// ********************
 // Buttons
+// ********************
+
 const btnStart = document.querySelector("#startbutton");
 
 const btnTest = document.querySelector("#TESTBUTTON");
 
-// Images
+// ********************
+// Images 
+// ********************
+
+// battle images
 let playerHpImage = document.querySelector(".player-hp-image");
 let playerDataImage = document.querySelector(".player-data-image");
 let opponentHpImage = document.querySelector(".opponent-hp-image");
 let attackImage = document.querySelector(".attack-image");
 
-// Div
+// choose pokemon images
+let grassIconImage = document.querySelector(".grass-icon-image");
+let grassPokeballImage = document.querySelector(".grass-pokeball-image");
+let grassEffectImage = document.querySelector(".grass-effect-image");
+let fireIconImage = document.querySelector(".fire-icon-image");
+let firePokeballImage = document.querySelector(".fire-pokeball-image");
+let fireEffectImage = document.querySelector(".fire-effect-image");
+let waterIconImage = document.querySelector(".water-icon-image");
+let waterPokeballImage = document.querySelector(".water-pokeball-image");
+let waterEffectImage = document.querySelector(".water-effect-image");
+
+
+// ********************
+// Divs
+// ********************
+
 const divStart = document.querySelector(".start-container");
 const divIntro = document.querySelector(".intro-container");
-const divContainer = document.querySelector(".container");
+const divContainer = document.querySelector(".container");  // overall game container
 
 // ======================================================================
 // Other variables
@@ -44,6 +66,50 @@ function transitionIntro() {
     divIntro.style.display = "none";   // show Intro div
 
     divContainer.style.display = "flex";    // show main game container
+}
+
+// ======================================================================
+// Choose Pokemon
+// ======================================================================
+// mouse hover on pokeball --> change poke icon image to color
+
+// grass pokeball
+grassPokeballImage.addEventListener('mouseover', () => {
+    grassIconImage.src = "images/choose-ui/venusaur-color.png";
+    grassEffectImage.style = "display: block";
+});
+grassPokeballImage.addEventListener('mouseout', () => {
+    grassIconImage.src = "images/choose-ui/venusaur-sepia.png";
+    grassEffectImage.style = "display: none";
+});
+
+// fire pokeball
+firePokeballImage.addEventListener('mouseover', () => {
+    fireIconImage.src = "images/choose-ui/charizard-color.png";
+    fireEffectImage.style = "display: block";
+});
+firePokeballImage.addEventListener('mouseout', () => {
+    fireIconImage.src = "images/choose-ui/charizard-sepia.png";
+    fireEffectImage.style = "display: none";
+});
+
+// water pokeball
+waterPokeballImage.addEventListener('mouseover', () => {
+    waterIconImage.src = "images/choose-ui/blastoise-color.png";
+    waterEffectImage.style = "display: block";
+});
+waterPokeballImage.addEventListener('mouseout', () => {
+    waterIconImage.src = "images/choose-ui/blastoise-sepia.png";
+    waterEffectImage.style = "display: none";
+});
+
+
+function colorPokemonIcon() {
+
+}
+
+function switchPokemonIconImage() {
+
 }
 
 // =============
